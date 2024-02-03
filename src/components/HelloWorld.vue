74fb97b61527c2f73519c6ce6a38e80f8cf73caf
Yevhenii <zheki2004@gmail.com>
2024-02-04 00:49:42 +0200

    feature: implemented pluralization, defaultLocale var, number formats

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,19 @@ import { ref } from 'vue'
 
 const { t } = useI18n()
 
+const dollarsCount = ref(100)
+
+const hundredDollarMessage = ref(t('Dollar', dollarsCount.value))
+
+const oneDollarMessage = ref(t('Dollar', dollarsCount.value - 99))
+
 const msg = ref(t('Hello'))
 </script>
 
 <template>
   <div>{{ msg }}</div>
+  <div>{{ hundredDollarMessage }}</div>
+  <div>{{ oneDollarMessage }}</div>
 </template>
 
 <style scoped></style>
