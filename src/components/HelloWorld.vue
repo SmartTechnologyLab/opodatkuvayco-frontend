321214161813600bb7f32f884f8d2e61ba43ba44
Yevhenii Zadorozhnyi <137901364+ZadorozhnyiYevhenii@users.noreply.github.com>
2024-02-04 23:32:20 +0200

    core: install i18n (#6)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-const msg = 'Hello'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>{{ msg }}</div>
+  <div>{{ $t('main.hello') }}</div>
+  <div>{{ $t('main.dollar', { n: 2 }) }}</div>
+  <div>{{ $t('main.dollar', { n: 0 }) }}</div>
+  <div>{{ $t('main.dollar', { n: 100 }) }}</div>
+  <p>{{ $d(new Date(), 'short', 'uk') }}</p>
+  <p>{{ $d(new Date(), 'long', 'uk') }}</p>
 </template>
 
 <style scoped></style>
