06544c3401356e65a66c13f4fc8f1b57245857dc
Poter <vitalii.p@unitedtech.ai>
2024-02-08 20:28:44 +0200

    fix: added temporary solution for i18n types

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { useTypedI18n } from '@/customHooks/useTypedI18n'
+import { useI18n } from 'vue-i18n'
 
-const { $t, $d } = useTypedI18n()
+// TODO: remove useI18N and use $t in template
+//  after resolving the issue https://github.com/intlify/vue-i18n-next/issues/1384
+const { t, d } = useI18n()
 </script>
 
 <template>
   <div>{{ $t('main.hello') }}</div>
-  <div>{{ $t('main.dollar', { n: 2 }) }}</div>
-  <div>{{ $t('main.dollar', { n: 0 }) }}</div>
-  <div>{{ $t('main.dollar', { n: 100 }) }}</div>
-  <p>{{ $d(new Date(), 'short', 'uk') }}</p>
-  <p>{{ $d(new Date(), 'long', 'uk') }}</p>
+  <div>{{ t('main.dollar', { n: 2 }) }}</div>
+  <div>{{ t('main.dollar', { n: 0 }) }}</div>
+  <div>{{ t('main.dollar', { n: 100 }) }}</div>
+  <p>{{ d(new Date(), 'short', 'uk') }}</p>
+  <p>{{ d(new Date(), 'long', 'uk') }}</p>
 </template>
 
 <style scoped></style>
--- src/customHooks/useTypedI18n.ts
+++
@@ -1,9 +0,0 @@
-import { useI18n } from 'vue-i18n'
-
-export const useTypedI18n = () => {
-  const { t, d } = useI18n()
-  const $t: (key: string, params?: Record<string, number>) => string = t
-  const $d: (date: Date, format: string, locale: string) => string = d
-
-  return { $t, $d }
-}
