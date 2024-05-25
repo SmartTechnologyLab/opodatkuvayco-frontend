fcddda1968417606271af6b827ac7439582b06d6
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-05-25 22:35:44 +0300

    refactor: remove props from UiImage

--- src/components/common/UiImage/UiImage.vue
+++ src/components/common/UiImage/UiImage.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import { useAttrs } from 'vue'
 
-const props = defineProps<{
-  src?: string
-  alt?: string
-}>()
-
 const attrs = useAttrs()
 </script>
 
 <template>
-  <img v-bind="{ ...props, ...attrs }" />
+  <img v-bind="{ ...attrs }" />
 </template>
 
 <style scoped></style>
