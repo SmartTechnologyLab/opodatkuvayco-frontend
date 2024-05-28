1839362940659e9a8457b296a6d60610fc24c0a9
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-05-29 00:53:45 +0300

    chore: init supabase/vue module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import generalConfig from '@/config'
+import VueSupabase from 'vue-supabase'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +12,10 @@ generalConfig(app)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueSupabase, {
+  supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
+  supabaseKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+  supabaseOptions: {}
+})
 
 app.mount('#app')
