82ea69ef1198d917a569bc04f6c85fc0149ecb25
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-05-30 01:14:52 +0300

    feat: optimize auth guard

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,10 +1,21 @@
 import type { Router } from 'vue-router'
 import { useSupabase } from 'vue-supabase'
+import { Routes } from './common'
 
 export const guard = (router: Router) => {
-  router.beforeEach(async (to) => {
+  router.beforeEach((to, _, next) => {
     const { auth } = useSupabase()
 
-    if (to.meta.requiresAuth && !auth.user()) return '/login'
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+      if (auth.user()) {
+        next()
+      } else {
+        next({
+          path: Routes.REGISTER_PAGE
+        })
+      }
+    } else {
+      next()
+    }
   })
 }
