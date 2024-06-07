96c86ee84d5aaccf8af8854d962349fd55a9d4f6
Yevhenii Zadorozhnyi <137901364+ZadorozhnyiYevhenii@users.noreply.github.com>
2024-06-07 22:00:16 +0300

    Test/mock router (#38)

---
+++ tests/setup-router-mock.ts
@@ -0,0 +1,16 @@
+import { VueRouterMock, createRouterMock, injectRouterMock } from 'vue-router-mock'
+import { config } from '@vue/test-utils'
+import { beforeEach, vi } from 'vitest'
+
+export const router = createRouterMock({
+  spy: {
+    create: vi.fn,
+    reset: vi.fn
+  }
+})
+beforeEach(() => {
+  router.reset()
+  injectRouterMock(router)
+})
+
+config.plugins.VueWrapper.install(VueRouterMock)
