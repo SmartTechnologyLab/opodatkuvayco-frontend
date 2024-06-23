3acf89f1d5eb91a321f8215c27def72453002559
Poter <poter925@gmail.com>
2024-06-23 16:10:48 +0300

    test: try to render mock scoped slots

--- tests/unit.setup.ts
+++ tests/unit.setup.ts
@@ -5,12 +5,12 @@ vi.mock('vue-i18n', () => ({
   useI18n: () => ({
     t: (key: string) => key,
     d: (date: Date) => date,
-    n: (num: number) => num
+    n: (num: number) => '%' + num + ' USD'
   })
 }))
 
 config.global.mocks = {
   $t: (msg: string) => msg,
   $d: (date: Date) => date,
-  $n: (num: number) => num
+  $n: (num: number) => num + ' USD'
 }
