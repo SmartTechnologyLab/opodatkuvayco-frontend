e91a9e55082f1d989a0e5eef32fce19290a09a33
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-07-26 19:24:44 +0300

    feat: added snapshots to tests TaxCalculator nad UiButton

--- src/components/common/UiButton/UiButton.test.ts
+++ src/components/common/UiButton/UiButton.test.ts
@@ -12,6 +12,14 @@ const router = createRouterMock({
   }
 })
 
+type Props = {
+  icon?: Icons
+  severity?: Severities
+  rounded?: boolean
+  outlined?: boolean
+  raised?: boolean
+}
+
 const ButtonStub = stubComponent(UiButton, {
   template: '<button><slot /></button>'
 })
@@ -19,7 +27,7 @@ const ButtonStub = stubComponent(UiButton, {
 describe('UiButton component', () => {
   let wrapper: VueWrapper<InstanceType<typeof UiButton>>
 
-  const createComponent = (props?: any, attrs?: Record<string, unknown>) => {
+  const createComponent = (props?: Props | null, attrs?: Record<string, unknown>) => {
     wrapper = shallowMount(UiButton, {
       props,
       attrs,
@@ -45,20 +53,12 @@ describe('UiButton component', () => {
 
   it('renders without props', () => {
     createComponent()
-    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.exists()).toMatchSnapshot()
   })
 
   it('renders with provided props', async () => {
     const CHECK_ICON = 'pi pi-check'
     const SEVERITY_DANGER = 'danger'
-
-    type Props = {
-      icon?: Icons
-      severity?: Severities
-      rounded?: boolean
-      outlined?: boolean
-      raised?: boolean
-    }
 
     const buttonProps = {
       icon: CHECK_ICON,
@@ -70,11 +70,11 @@ describe('UiButton component', () => {
 
     createComponent(buttonProps)
 
-    expect(wrapper.props().icon).toBe(buttonProps.icon)
-    expect(wrapper.props().severity).toBe(buttonProps.severity)
-    expect(wrapper.props().rounded).toBe(buttonProps.rounded)
-    expect(wrapper.props().outlined).toBe(buttonProps.outlined)
-    expect(wrapper.props().raised).toBe(buttonProps.raised)
+    expect(wrapper.props().icon).toMatchSnapshot()
+    expect(wrapper.props().severity).toMatchSnapshot()
+    expect(wrapper.props().rounded).toMatchSnapshot()
+    expect(wrapper.props().outlined).toMatchSnapshot()
+    expect(wrapper.props().raised).toMatchSnapshot()
   })
 
   it('emits click event when button get clicked', async () => {
@@ -82,13 +82,13 @@ describe('UiButton component', () => {
 
     await wrapper.trigger('click')
 
-    expect(wrapper.emitted()).toHaveProperty('click')
+    expect(wrapper.emitted()).toMatchSnapshot()
   })
 
   it('passes value to defaul slot', () => {
     createComponent()
 
-    expect(wrapper.text()).toContain('Submit')
+    expect(wrapper.text()).toMatchSnapshot()
   })
 
   const TEST_ROUTE = '/home'
@@ -101,8 +101,8 @@ describe('UiButton component', () => {
 
     await wrapper.router.push(TEST_ROUTE)
 
-    expect(wrapper.findComponent({ name: 'RouterLink' })).toBeDefined()
-    expect(wrapper.router.currentRoute.value.path).toEqual(TEST_ROUTE)
+    expect(wrapper.findComponent(RouterLinkStub).exists()).toMatchSnapshot()
+    expect(wrapper.router.currentRoute.value.path).toMatchSnapshot()
   })
 
   it('have not attribute class on element router link', async () => {
@@ -116,7 +116,7 @@ describe('UiButton component', () => {
       }
     })
 
-    expect(wrapper.findComponent({ name: 'RouterLink' }).attributes().class).toBeUndefined()
+    expect(wrapper.findComponent({ name: 'RouterLink' }).attributes().class).toMatchSnapshot()
   })
 
   const HREF_TEST = '#about'
@@ -128,7 +128,7 @@ describe('UiButton component', () => {
       }
     })
 
-    expect(wrapper.find('a').attributes().href).toEqual(HREF_TEST)
+    expect(wrapper.find('a').attributes().href).toMatchSnapshot()
   })
 
   it('have not attribute class on element a', async () => {
@@ -142,7 +142,7 @@ describe('UiButton component', () => {
       }
     })
 
-    expect(wrapper.find('a').attributes().class).toBeUndefined()
-    expect(wrapper.findComponent({ name: 'Button' }).attributes().class).toEqual(CLASS_BTN)
+    expect(wrapper.find('a').attributes().class).toMatchSnapshot()
+    expect(wrapper.findComponent({ name: 'Button' }).attributes().class).toMatchSnapshot()
   })
 })
