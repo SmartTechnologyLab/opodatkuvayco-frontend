944720cac19d78ff6686d505eb32547d61cc193b
ZadorozhnyiYevhenii <yevhenii.zadorozhnyi.dev@gmail.com>
2024-09-12 23:45:02 +0300

    feat: vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import checker from 'vite-plugin-checker'
+import VueDevtools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevtools(),
     eslint({
       emitWarning: true,
       exclude: [/virtual:/, /node_modules/, /sb-preview/]
