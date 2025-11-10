924da7ee4cb49f810819894df5fdf0f84b1bd670
Poter <poter925@gmail.com>
2025-11-10 18:47:35 +0200

    test: correct date formatting in HelloWorld test

--- src/components/HelloWorld.test.ts
+++ src/components/HelloWorld.test.ts
@@ -7,7 +7,7 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld)
 
     expect(wrapper.text()).toMatch(
-      'Hello2 долари0 доларів100 доларів10 000,00 USD10 000,00 EUR10 000,00 ₴8 лист. 2000 р.середу, 8 листопада 2000 р. о 00:00'
+      'Hello2 долари0 доларів100 доларів10 000,00 USD10 000,00 EUR10 000,00 ₴8 лист. 2000 р.середа, 8 листопада 2000 р. о 00:00'
     )
   })
 })
