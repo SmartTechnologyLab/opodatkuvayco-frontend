4d3f81f530e27e1f2c1f054d024ea2fd26d9957e
Poter <poter925@gmail.com>
2025-11-10 19:09:07 +0200

    ci: update NODE_VERSION to use variable and remove timezone settings

--- tests/unit.setup.ts
+++ tests/unit.setup.ts
@@ -1,9 +1,6 @@
 import { config } from '@vue/test-utils'
 import { i18n } from '../src/i18n'
 
-// Set timezone to ensure consistent date formatting across environments
-process.env.TZ = 'Europe/Kiev'
-
 config.global.mocks = {
   $t: (msg: string) => msg,
   $d: (date: Date) => date,
